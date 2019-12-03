3c7cbe421d43eb6db2dfc8a2753f0fd1ca9461e5
kostek895 <kamil.kostka@wp.pl>
2019-12-03 20:55:13 +0100

    update config

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect('localhost','root','','real_estate')or die(mysqli_error());
+$con=mysqli_connect('mysql-660824.vipserv.org','makeup_projekt','R2cblgGSJI19Eknhy3ZSYPiTg1Om01tS','makeup_projekt')or die(mysqli_error());
 
 ?>
