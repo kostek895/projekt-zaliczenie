8f0cd8aa8abaaa539aabe8c041d64f251f8118d7
kostek895 <kamil.kostka@wp.pl>
2019-12-04 19:40:03 +0100

    update

--- include/header.php
+++ include/header.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="zxx">
+﻿<!DOCTYPE html>
+<html lang="pl">
 <head>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        '../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PDTWJ3Z');</script>
-    <!-- End Google Tag Manager -->
-    <title> property Dealer</title>
+    <title>Projekt na zaliczenie - KOSTKA 57136</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <!-- External CSS libraries -->
@@ -38,12 +31,6 @@
 
 </head>
 <body id="top">
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDTWJ3Z"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) 
-<div class="page_loader"></div>
--->
 <!-- main header start -->
 <header class="main-header sticky-header" id="main-header-2">
     <div class="container">
@@ -51,8 +38,7 @@
             <div class="col-12">
                 <nav class="navbar navbar-expand-lg navbar-light rounded">
                     <a class="navbar-brand logo navbar-brand d-flex w-50 mr-auto" href="index.php">
-                        <img src="assets/img/logos/black-logo.png" alt="logo">
-
+                        Projekt na zaliczenie
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="fa fa-bars"></span>
@@ -60,27 +46,12 @@
                     <div class="navbar-collapse collapse w-100" id="navbar">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item dropdown active">
-                                <a class="nav-link dropdown-toggle" href="index.php">Home</a>
+                                <a class="nav-link dropdown-toggle" href="index.php">Wyświetlanie danych</a>
                             </li>
 							
 							<li class="nav-item dropdown megamenu-li">
-                                <a class="nav-link dropdown-toggle" href="about.php">About</a>
+                                <a class="nav-link dropdown-toggle" href="admin/add_property.php">Dodawanie danych</a>
                             </li>
-							
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="properties.php">Properties</a>
-                            </li>
-							                                                        
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="contact.php">Contact</a>
-                            </li>
-                            
-							<li class="nav-item dropdown">
-                                <a href="#full-page-search" class="nav-link">
-                                    <i class="fa fa-search"></i>
-                                </a>
-                            </li>
-                            
 													
                         </ul>
                     </div>
