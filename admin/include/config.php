002dd17888a72f9e87de3d81eadd2dc8a90c5fd8
kostek895 <kamil.kostka@wp.pl>
2019-12-06 16:13:58 +0100

    update

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,9 +1,4 @@
 <?php
-$con=mysqli_connect('zaliczenie-kostka.mysql.database.azure.com','kostka@zaliczenie-kostka','zaq1@WSX','zaliczenie');
-if ($con -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $con -> connect_error;
-  exit();
-} else {
-	echo "Wszystko git";
-}
+$con=mysqli_connect('zaliczenie-kostka.mysql.database.azure.com','kostka@zaliczenie-kostka','zaq1@WSX','zaliczenie')or die(mysqli_error());
+
 ?>
